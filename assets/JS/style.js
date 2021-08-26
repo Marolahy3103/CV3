6f98d5d27b7883f22c72f359f890a16939e72152
marol <randriamarolahypierre@gmail.com>
2021-08-26 10:01:43 +0300

    Upload CV 26/08/2021

---
+++ assets/JS/style.js
@@ -0,0 +1,117 @@
+function showOver()
+{
+	document.getElementById("html-csss").style.display="block" ;
+}
+
+/* script js pour cacher l'imge college*/
+function showUp()
+{
+ 	document.getElementById("html-csss").style.display="none";
+ 
+}
+
+function showOverjs()
+{
+	document.getElementById("javascript").style.display="block" ;
+}
+
+/* script js pour cacher l'imge college*/
+function showUpjs()
+{
+ 	document.getElementById("javascript").style.display="none";
+ 
+}
+
+function showOverphp()
+{
+	document.getElementById("PHP").style.display="block" ;
+}
+
+/* script js pour cacher l'imge college*/
+function showUpphp()
+{
+ 	document.getElementById("PHP").style.display="none";
+ 
+}
+
+function showOverPYTHON()
+{
+	document.getElementById("python").style.display="block" ;
+}
+
+/* script js pour cacher l'imge college*/
+function showUpPYTHON()
+{
+ 	document.getElementById("python").style.display="none";
+ 
+}
+
+function showOverSQL()
+{
+	document.getElementById("sql").style.display="block" ;
+}
+
+function showUpSQL()
+{
+ 	document.getElementById("sql").style.display="none";
+ 
+}
+
+function showOverOracle()
+{
+	document.getElementById("oracle").style.display="block" ;
+}
+
+function showUpOracle()
+{
+ 	document.getElementById("oracle").style.display="none";
+ 
+}
+
+
+function showOverReseau()
+{
+	document.getElementById("reseau").style.display="block" ;
+}
+
+function showUpReseau()
+{
+ 	document.getElementById("reseau").style.display="none";
+ 
+}
+
+
+function showOverCsfx()
+{
+	document.getElementById("csfx").style.display="block" ;
+}
+
+function showUpCsfx()
+{
+ 	document.getElementById("csfx").style.display="none";
+ 
+}
+
+
+function showOverEsti()
+{
+	document.getElementById("esti").style.display="block" ;
+}
+
+function showUpEsti()
+{
+ 	document.getElementById("esti").style.display="none";
+ 
+}
+
+
+function showOverSesame()
+{
+	document.getElementById("sesame").style.display="block" ;
+}
+
+function showUpSesame()
+{
+ 	document.getElementById("sesame").style.display="none";
+ 
+}
